5af67279ddae1e33707f421f0b2163f782289ee9
Carlos Armando <armandomurillo803@gmail.com>
2022-06-04 14:31:13 -0600

    ejercicio 17

---
+++ EJERCICIOS/Ejercicio 17 Menu con Switch/Ejercicio21-Menu con Switch.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int opcion = 0;
+	
+	while(true) {
+		system("cls");
+		cout <<"*****" <<endl;
+		cout <<"MENU" <<endl;
+		cout <<"****" <<endl;
+		
+		cout <<endl;
+		cout << "1 - Cafe y Granitas " <<endl;
+		cout << "2 - Reposteria" <<endl;
+		cout << "o - Salir" <<endl;
+		
+		cout << "Ingrese una opcion del menu: ";
+		cin >> opcion;
+		
+			if (opcion == 0){
+			break;
+		}
+		
+		switch (opcion)
+		{
+	        case 1:
+	    	{
+	    	    system("cls");
+			    cout << "Estas en el menu de cafe y granitas" <<endl;
+			    system("pause");	
+			    break;
+			}
+	        case 2:
+	        {
+	        	system("cls");
+			    cout << "Estas en el menu de Reposteria" <<endl;
+			    system("pause");
+			    break;
+			}
+		
+	    	
+	    default:
+	    {
+	    	cout << "Ingrese una opcion valida (0, 1 , 2 )";
+	    	system("pause");
+	    	break;
+		}
+	
+		
+		}
+
+	}
+
+    
+
+
+    return 0;
+}
+
+
