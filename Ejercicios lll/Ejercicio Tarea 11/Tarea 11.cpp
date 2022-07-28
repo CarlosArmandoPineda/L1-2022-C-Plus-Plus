67754b723b2715262c2bc7711e1bb1f0f67ad26c
armandomurillo803@gmail.com <armandomurillo803@gmail.com>
2022-07-28 10:44:24 -0600

    tarea

---
+++ Ejercicios lll/Ejercicio Tarea 11/Tarea 11.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int random()
+{
+    return rand() % 5 + 1;
+}
+
+int main(int argc, char** argv)
+{
+
+    int random();
+	srand(time(NULL));
+	int suma = 0;
+	int numero [5][5];
+	
+	for (int i = 0; i < 5; i++)
+        
+		for (int j = 0; j < 5; j++)
+        {
+        	numero[i][j] = random();
+        	suma = suma + numero[i][j];
+        }
+        
+    cout <<"Suma con Arreglo Bidimensional ";
+    cout <<endl;
+    cout <<endl;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+        	cout << "  " <<numero[i][j];
+        }
+        cout <<endl;
+    }
+    
+    cout <<endl;
+    cout<< "La suma es : " << suma <<endl;
+	
+	return 0;
+}
+
+
